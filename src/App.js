f9f4f11cca897fb92c3e7b27db4401cd5e4853d2
Arhum2 <arhumshahzad2003@gmail.com>
2021-12-31 02:36:55 -0500

    created expense item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>hi tiktok!!!!</h2>
-      <p>This is also visable</p>
+      <h2>Expense Tracker</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>November 3rd 2021</div>
+      <div>
+        <h2>Netflex subscription</h2>
+        <div>$16.99</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
