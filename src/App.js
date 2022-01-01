c83987c95fe7666140b88f4d75283a600bb5682e
Arhum2 <Arhumshahzad2003@gmail.com>
2021-12-31 23:14:02 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: 'Care Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+    { title: 'Care Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+    { title: 'Care Insurance', amount: 294.67, date: new Date(2021, 2, 28)},
+  ];
   return (
     <div>
       <h2>Expense Tracker</h2>
+      <ExpenseItem></ExpenseItem>
       <ExpenseItem></ExpenseItem>
     </div>
   );
