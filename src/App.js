85557e37fa56f203e7e942adbdf65471fadaf28d
Arhum2 <arhumshahzad2003@gmail.com>
2022-01-02 03:40:06 -0500

    created dates on expenses

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
+      title: "Netflex Subscription",
+      amount: 16.99,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.99, date: new Date(2021, 2, 12) },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
+      title: "Candles",
+      amount: 999.99,
       date: new Date(2021, 2, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
+      title: "Ikea",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
