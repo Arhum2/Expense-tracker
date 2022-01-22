6d67148aadd3133bd11010abecc588fbe01dcbd2
Arhum2 <arhumshahzad2003@gmail.com>
2022-01-22 03:22:50 -0500

    Created no expenses found notification

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -3,11 +3,9 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = (props) => {
-  let expensesContent = <p>No expenses found.</p>;
-
-    if (props.items.length === 0) {
-        return <h2 className="expenses-list__fallback">found no expenses</h2>
-    }
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
+  }
 
   return (
     <ul className="expenses-list">
