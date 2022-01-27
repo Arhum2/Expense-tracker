3194299b3bf61061a97e90f278ff896e52209b8c
Arhum2 <arhumshahzad2003@gmail.com>
2022-01-27 01:50:04 -0500

    created logic for bar height

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import ChartBar from './ChartBar';
-import './Chart.css';
+import ChartBar from "./ChartBar";
+import "./Chart.css";
 
-const Chart = prop => {
-    return <div className='chart'></div>
+const Chart = (prop) => {
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
